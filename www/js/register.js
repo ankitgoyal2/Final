fedd018ad1b85f4f2d74818f818054c0b9096e92
Ankit_Goyal <goyala960@gmail.com>
2021-05-18 16:54:32 +0530

    emergency root added in patient(error in api call)

--- www/js/register.js
+++ www/js/register.js
@@ -37,8 +37,9 @@ $(function(){
 				
 			}
 			
-	});
+	);
 });
+
 
 // $("form").on("change", ".file-upload-field", function(){ 
 //     $(this).parent(".file-upload-wrapper").attr("data-text",         $(this).val().replace(/.*(\/|\\)/, '') );
